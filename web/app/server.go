15c2a55ff3aab47767fa139b2cab9d96c7515529
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-09-28 22:15:59 +0100

    feat: mkdir -p when server starts

--- web/app/server.go
+++ web/app/server.go
@@ -2,17 +2,24 @@ package app
 
 import (
 	"net/http"
+	"os"
 	"pliki-server/configs"
 	"strconv"
 )
 
 func CreateServer(cfg *configs.Config) error {
-	router := createRouter(cfg)
-	port := ":" + strconv.Itoa(cfg.Port)
-
-	err := http.ListenAndServe(port, router)
+	err := os.MkdirAll(cfg.FileServerRoot, 0755)
 	if err != nil {
 		return err
 	}
+
+	router := createRouter(cfg)
+	port := ":" + strconv.Itoa(cfg.Port)
+
+	err = http.ListenAndServe(port, router)
+	if err != nil {
+		return err
+	}
+
 	return nil
 }
