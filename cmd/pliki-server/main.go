f8843e3e51e4f2efa077651ba3b90cf048127afa
Pedro Barco <pedro.barco@tecnico.ulisboa.pt>
2021-09-28 22:16:37 +0100

    feat: parse env vars when starting server

--- cmd/pliki-server/main.go
+++ cmd/pliki-server/main.go
@@ -2,12 +2,31 @@ package main
 
 import (
 	"log"
+	"os"
 	"pliki-server/configs"
 	"pliki-server/web/app"
+	"strconv"
 )
 
 func main() {
 	cfg := configs.GetDefaultConfig()
+
+	envPort := os.Getenv("PLIKI_PORT")
+	envFileServerRoot := os.Getenv("PLIKI_FILE_SERVER_ROOT")
+	envMaxFileSizeMb := os.Getenv("PLIKI_MAX_FILE_SIZE_MB")
+
+	if envPort != "" {
+		cfg.Port, _ = strconv.Atoi(envPort)
+	}
+
+	if envFileServerRoot != "" {
+		cfg.FileServerRoot = envFileServerRoot
+	}
+
+	if envMaxFileSizeMb != "" {
+		cfg.MaxFileSizeMb, _ = strconv.ParseInt(envMaxFileSizeMb, 10, 64)
+	}
+
 	err := app.CreateServer(cfg)
 	if err != nil {
 		log.Fatal(err)
